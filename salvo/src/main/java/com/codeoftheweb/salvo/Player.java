afb94fea4694680865d75e718bec39df168199ae
catherinesofio <catalinasofioavogadro@hotmail.com>
2019-07-27 23:55:15 -0300

    Task 01

---
+++ salvo/src/main/java/com/codeoftheweb/salvo/Player.java
@@ -0,0 +1,52 @@
+package com.codeoftheweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Player() { }
+
+    public Player(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
---
+++ salvo/src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -0,0 +1,25 @@
+package com.codeoftheweb.salvo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository repository){
+		return args -> {
+			repository.save(new Player("Jack Bauer", "j.bauer@ctu.gov", "24"));
+			repository.save(new Player("Chloe O'Brian", "c.obrian@ctu.gov", "42"));
+			repository.save(new Player("Kim Bauer", "kim_bauer@gmail.com", "kb"));
+			repository.save(new Player("Tony Almeida", "t.almeida@ctu.gov", "mole"));
+		};
+	}
+
+}
